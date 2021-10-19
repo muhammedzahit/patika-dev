c9ce81b61dba73affc9838b2e0e22f625200279b
muhammedzahit <55826843+muhammedzahit@users.noreply.github.com>
2021-10-19 03:32:11 +0300

    nodejs patikası ödev 3

---
+++ nodejs-odev3/main.js
@@ -0,0 +1,6 @@
+const {circleArea, circleCircumference} = require("./geo")
+
+const yaricap = 12
+console.log("yaricap", yaricap)
+console.log("alan", circleArea(yaricap))
+console.log("cevre", circleCircumference(yaricap))
